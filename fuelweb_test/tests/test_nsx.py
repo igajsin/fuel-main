dc5e40c048543dc46ad6a7cd2073f70c24172151
Igor Gajsin <igajsin@mirantis.com>
2014-07-29 20:20:48 +0400

    change cluster_id settings for nsx
    Change-Id: I2c5a1918a4b65b7da2026c240709d2fdaf1df861

--- fuelweb_test/tests/test_nsx.py
+++ fuelweb_test/tests/test_nsx.py
@@ -4,6 +4,7 @@ from fuelweb_test.helpers.decorators import log_snapshot_on_error
 from fuelweb_test.tests.base_test_case import SetupEnvironment
 from fuelweb_test.tests.base_test_case import TestBasic
 from fuelweb_test.settings import DEPLOYMENT_MODE_SIMPLE
+import fuelweb_test.settings as help_data
 from fuelweb_test import logger
 from proboscis import test
 
@@ -22,7 +23,7 @@ class NsxDeploy(TestBasic):
             3. Add 1 node with cinder role
             4. Add 1 node with compute role
             5. Deploy the cluster
-            6. Create and delete instance
+            6. Run ostf test which check connectivity to the floating IP using ping command.
         """
         logger.info("DBG! deploy_nsx start")
         self.env.revert_snapshot("ready_with_3_slaves")
@@ -32,10 +33,19 @@ class NsxDeploy(TestBasic):
             name=self.__class__.__name__,
             mode=DEPLOYMENT_MODE_SIMPLE,
             settings={
+                'libvirt_type': 'qemu',
+                'nsx_username': 'admin',
+                'nsx_password': 'admin',
+                'transport_zone_uuid': 'e0c69125-86cc-4118-888f-90dae8a5e1e8',
+                'l3_gw_service_uuid': '365555e0-0400-46a9-b8c2-f02ab511220c',
+                'nsx_controllers': '172.16.1.253',
+                'packages_url': '172.16.1.1',
+                'connector_type': 'stt',
                 'tenant': 'novaSimpleFlat',
                 'user': 'novaSimpleFlat',
                 'password': 'novaSimpleFlat'
-            }
+            },
+            release_name=help_data.OPENSTACK_RELEASE
         )
         logger.info('cluster is %s' % str(cluster_id))
         self.fuel_web.update_nodes(
