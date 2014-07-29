2a5bfa42a1b43f0f5a160e5532a8f5a91bc766c9
Igor Gajsin <igajsin@mirantis.com>
2014-07-29 14:06:12 +0400

    rewrite deploy_nsx
    Change-Id: Ic108890ba97c9046173a7c939fde8a29ff2aad62

--- fuelweb_test/tests/test_nsx.py
+++ fuelweb_test/tests/test_nsx.py
@@ -3,6 +3,7 @@ import proboscis
 from fuelweb_test.helpers.decorators import log_snapshot_on_error
 from fuelweb_test.tests.base_test_case import SetupEnvironment
 from fuelweb_test.tests.base_test_case import TestBasic
+from fuelweb_test.settings import DEPLOYMENT_MODE_SIMPLE
 from fuelweb_test import logger
 from proboscis import test
 
@@ -28,10 +29,13 @@ class NsxDeploy(TestBasic):
         self.fuel_web.client.get_root()
         self.env.bootstrap_nodes(self.env.nodes().slaves[:1])
         cluster_id = self.fuel_web.create_cluster(
-            name=self.__class__.__name__
-            # mode=settings{
-            # 'nsx':'aaa'
-            # }
+            name=self.__class__.__name__,
+            mode=DEPLOYMENT_MODE_SIMPLE,
+            settings={
+                'tenant': 'novaSimpleFlat',
+                'user': 'novaSimpleFlat',
+                'password': 'novaSimpleFlat'
+            }
         )
         logger.info('cluster is %s' % str(cluster_id))
         self.fuel_web.update_nodes(
@@ -42,6 +46,13 @@ class NsxDeploy(TestBasic):
                 'slave-03': ['compute']
             }
         )
+        logger.info('DBG! deploy_cluster_wait')
         self.fuel_web.deploy_cluster_wait(cluster_id)
+        logger.info('\e[32mDBG! run ostf\033[0m')
         # Run ostf
         self.fuel_web.run_ostf(cluster_id=cluster_id)
+        self.fuel_web.run_single_ostf_test(
+            cluster_id=cluster_id, test_sets=['smoke'],
+            test_name=('fuel_health.tests.smoke.'
+                       'test_nova_create_instance_with_connectivity.'
+                       'TestNovaNetwork.test_004_create_servers'))
